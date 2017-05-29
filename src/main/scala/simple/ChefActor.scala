3ff729b31d698adae91fe14551fde2b137fd1cfb
worasit daimongkol <worasit.dmk501@gmail.com>
2017-05-29 18:08:17 +0700

    Defined a ChefActor

--- src/main/scala/simple/ChefActor.scala
+++ src/main/scala/simple/ChefActor.scala
@@ -1,6 +1,25 @@
 package simple
 
+import akka.actor.Actor
 
-class ChefActor {
 
+// =========== Items Enumeration ===========
+sealed trait Item
+
+case object Pizza extends Item
+
+case object Omelet extends Item
+
+
+// =========== Actor ===========
+class ChefActor extends Actor {
+  override def receive: Receive = {
+    case Pizza => println("Pizza is delivered!")
+    case Omelet => println("Omelet is delivered")
+    case _ => println("This item does not exist.")
+  }
 }
+
+
+
+
